f3748d60685621117eac08ea0d8abcd8b0389033
razmiO <thomas.cuesta31@gmail.com>
2024-12-18 00:00:55 +0100

    Add suppport for html ?

--- src/App.ts
+++ src/App.ts
@@ -1,39 +1,112 @@
+import term from "@/term.ts";
+
 type ContextExtension = {
     path: string;
-    groups: Record<string, string>;
+    target: string;
+    res: Response;
 };
 type RequestContext = Request & ContextExtension & Record<string, string>;
 type Handler = (req: RequestContext) => Response;
+type RouteHandler = (targetPath: string, handler: Handler) => App;
 
-export default class App {
-    constructor(public req: Request, public routes: Map<string, { method: string; handler: Handler }> = new Map()) {}
+type Methods = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
 
-    public get(path: string, handler: Handler): void {
-        this.routes.set(path, { method: "GET", handler: handler });
+class Routes {
+    constructor(
+        protected req: Request,
+        /**
+         * Default response
+         * ```ts
+         * new Response("Not found", {
+         *    status: 404,
+         * });
+         * ```
+         */
+        protected res = new Response("Not found", {
+            status: 404,
+            headers: {
+                "Content-Type": "text/plain",
+            },
+        }),
+        protected routes: Map<
+            `${Methods}-${string}`,
+            { method: Methods; handler: Handler; targetPath: string }
+        > = new Map(),
+        protected verbose = false
+    ) {}
+
+    protected addRoute = (method: Methods, targetPath: string, handler: Handler): this => {
+        this.routes.set(`${method}-${targetPath}`, { method, handler, targetPath });
+        this.verbose && console.log(term("green", ` - Added ${method.padEnd(5)} ${targetPath}`));
+        return this;
+    };
+}
+
+export default class App extends Routes {
+    constructor(req: Request) {
+        super(req);
     }
+    public get = ((targetPath, handler) => this.addRoute("GET", targetPath, handler)) as RouteHandler;
+    public post = ((targetPath, handler) => this.addRoute("POST", targetPath, handler)) as RouteHandler;
+    public put = ((targetPath, handler) => this.addRoute("PUT", targetPath, handler)) as RouteHandler;
+    public delete = ((targetPath, handler) => this.addRoute("DELETE", targetPath, handler)) as RouteHandler;
+    public patch = ((targetPath, handler) => this.addRoute("PATCH", targetPath, handler)) as RouteHandler;
 
     public run(): Response {
-        console.log("run");
-        for (const [path, { method, handler }] of this.routes) {
-            const pattern = new URLPattern({ pathname: path }); // { pathname: "/books/:user" }
-            const match = pattern.exec(new URL(this.req.url).pathname);
-            console.log("compared : ", path, " AND ", new URL(this.req.url).pathname, " RESULTING IN ", match);
+        this.verbose && console.log(` - ${this.routes.size} routes`);
+        for (const [_key, { method, handler, targetPath }] of this.routes) {
+            const pattern = new URLPattern({ pathname: targetPath }); // { pathname: "/books/:user" }
+            const match = pattern.exec(this.req.url);
+
             if (match && this.req.method === method) {
-                const pattern = new URLPattern({ pathname: path }); // { pathname: "/books/:user" }
-                const match = pattern.exec(new URL(this.req.url).pathname);
+                this.verbose &&
+                    console.log(term("blue", ` - Matched ${this.req.method} ${targetPath} => ${match.pathname.input}`));
 
                 const context = {
                     ...this.req,
-                    path: path,
-                    groups: match?.pathname.groups ?? {},
+                    ...match.pathname.groups,
+                    path: match.pathname.input,
+                    target: targetPath,
+                    res: this.res,
                 } as RequestContext;
 
                 return handler(context);
             }
         }
 
-        return new Response("Not found", {
+        this.verbose && console.log(term("red", `${this.req.method.padEnd(5)} ${this.req.url} => Not found`));
+
+        return this.res;
+    }
+
+    // --
+    // --
+    // -- HELPERS
+    // --
+    // --
+
+    setVerbose(debug: boolean): this {
+        this.verbose = debug;
+        return this;
+    }
+
+    jsonResponse(): this {
+        this.res = new Response(JSON.stringify({ message: "404 Note found" }), {
             status: 404,
+            headers: {
+                "Content-Type": "application/json",
+            },
         });
+        return this;
+    }
+
+    htmlResponse(): this {
+        this.res = new Response("<h1>404 Not found</h1>", {
+            status: 404,
+            headers: {
+                "Content-Type": "text/html",
+            },
+        });
+        return this;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,9 @@
 import App from "@/App.ts";
-import term from "@/term.ts";
 
 const json = (data: Record<string, string>): string => {
     return JSON.stringify(data);
 };
 
-// const app = new App();
-
-// app.get("/books/:user", ({ user, path }) => {
-//     console.log(term("blue", `GET ${path}`));
-//     return new Response(json({ user }), {
-//         status: 200,
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//     });
-// });
-
-// Deno.serve({ port: 8080, hostname: "127.0.0.1" }, app.run);
 const html = `
 <!DOCTYPE html>
 <html lang="en">
@@ -34,30 +20,50 @@ const html = `
 
 export default {
     fetch(req: Request) {
-        const app = new App(req);
+        // #
+        // #
+        // #
+        // #
+        // #
+        return (
+            new App(req)
+                .setVerbose(true)
+                .htmlResponse()
+                //
+                .get("/", ({ _user, _res }) => {
+                    return new Response(html, {
+                        status: 200,
+                        headers: {
+                            "Content-Type": "text/html",
+                        },
+                    });
+                })
+                .post("/books/:user", ({ user, _res }) => {
+                    return new Response(json({ user }), {
+                        status: 200,
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    });
+                })
+                .put("/books/:user", ({ user, _res }) => {
+                    return new Response(json({ user }), {
+                        status: 200,
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    });
+                })
+                .run()
+        );
 
-        app.get("/books/:user", (context) => {
-            const { user, path } = context;
-            console.log(term("blue", `GET ${path} ${user} ${JSON.stringify(context)}`));
-            return new Response(json({ user }), {
-                status: 200,
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-        });
-
-        return app.run();
+        // #
+        // #
+        // #
+        // #
+        // #
     },
 };
-
-// return new Response(html, {
-//     status: 200,
-//     headers: {
-//         "content-type": "text/html",
-//     },
-// });
-// (req: Request) => Response
 
 // // note :
 // // - des demain : ok
